d8e113193e0da5db9f4a6f8f9156581ee66247d5
gangelion <gangelion8@gmail.com>
2020-04-28 11:11:13 +0900

    update top_page

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -13,44 +13,52 @@
     </div>
     <div class="row">
       <div class="menu_list col-12 d-flex text-center my-5">
-        <div class="menu col-2 text-center">Profile</div>
-        <div class="menu col-2 text-center">Portfolio</div>
-        <div class="menu col-2 text-center">Blog</div>
-        <div class="menu col-2 text-center">Skill</div>
-        <div class="menu col-2 text-center">Github</div>
-        <div class="menu col-2 text-center">Contact</div>
+        <a href="#" class="menu col-2 text-center">Profile</a>
+        <a href="#" class="menu col-2 text-center">Portfolio</a>
+        <a href="#" class="menu col-2 text-center">Blog</a>
+        <a href="#" class="menu col-2 text-center">Skill</a>
+        <a href="https://github.com/gangelion" class="menu col-2 text-center">Github</a>
+        <a href="#" class="menu col-2 text-center">Contact</a>
       </div>
     </div>
     <div class="row">
-      <h2 class="profile col-12 text-center">Profile</h2>
-      <div class="d-flex">
+      <h2 class="profile col-12 text-center my-5">Profile</h2>
+      <div class="d-flex col-12">
         <div class="profile_content col-6">プロフィール内容</div>
         <div class="profile_image col-6">
-          <img src="storage/app/public/T1MLERC4C-UR31TJGLT-bffc2f115b74-512.jpg" alt="image">
+          <img src="{{ asset('img/T1MLERC4C-UR31TJGLT-bffc2f115b74-512.jpg') }}" alt="プロフィール画像" style="width: 300px; height: 300px; border-radius: 5px;">
         </div>
       </div>
     </div>
     <div class="row">
-      <h2 class="portfolio col-12 text-center">Portfolio</h2>
-      <div>firstskillapp
-        <a href="https://firstskillapp.com">firstskillapp</a>
+      <h2 class="portfolio col-12 text-center my-5">Portfolio</h2>
+      <h3 class="col-12 text-center mb-5">firstskillapp
+        <a href="https://firstskillapp.com">https://firstskillapp.com</a>
+      </h3>
+      <img class="col-4" src="{{ asset('img/screencapture-18-178-33-110-2020-03-27-11_45_19.png') }}" alt="ポートフォリオ" style="width: 300px; height: 300px; border-radius: 5px;">
+      <img class="col-4" src="{{ asset('img/screencapture-18-178-33-110-users-2020-03-27-11_47_18.png') }}" alt="ポートフォリオ" style="width: 300px; height: 300px; border-radius: 5px;">
+      <img class="col-4" src="{{ asset('img/screencapture-localhost-3000-plans-new-2020-03-22-23_00_51.png') }}" alt="ポートフォリオ" style="width: 300px; height: 300px; border-radius: 5px;">
+    </div>
+    <div class="row">
+      <h2 class="blog col-12 text-center my-5">Blog</h2>
+      <div class="blog_list col-12 d-flex text-center">
+        <div class="article col-6">ブログ記事1</div>
+        <div class="article col-6">ブログ記事2</div>
       </div>
     </div>
     <div class="row">
-      <h2 class="blog col-12 text-center">Blog</h2>
-      <div class="blog_list d-flex">
-        <div class="col-6">ブログ記事1</div>
-        <div class="col-6">ブログ記事2</div>
-      </div>
-    </div>
-    <div class="row">
-      <h2 class="skill col-12 text-center">Skill</h2>
+      <h2 class="skill col-12 text-center my-5">Skill</h2>
       <div class="skill_list">
         <div>Ruby on Rails</div>
+        <div>Laravel</div>
+        <div>JQuery</div>
+        <div>Docker</div>
+        <div>CircleCI</div>
+        <div>AWS</div>
       </div>
     </div>
     <div class="row">
-      <h2 class="contact col-12 text-center">Contact</h2>
+      <h2 class="contact col-12 text-center my-5">Contact</h2>
     </div>
   </div>
 </body>
